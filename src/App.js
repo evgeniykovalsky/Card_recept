7bb00936755f17929fe563abebf0da9f04bb4b4c
Evgeniy <evgeniykovalsky04386@gmail.com>
2022-08-12 21:41:54 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+export default class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            description:false
+        };
+
+        //const[description,setDescription]= useState(false)
+    }
+
+render(){
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+       <div className="conteiner">
+    <div className="head">{this.props.data.name}</div>
+
+    <div className="mainBody">
+        <div className="picture"> 
+        <img src={this.props.data.image3} alt={this.props.data.id}/>
+        </div>
+        <div className="description">
+            <p><span>Назва страви :</span> {this.props.data.name}</p>
+            <p><span>Інгредієнти для приготування:</span> </p>
+           <p> <ul>
+                <li>{this.props.data.ingredient1}</li>
+                <li>{this.props.data.ingredient2}</li>
+                <li>{this.props.data.ingredient3}</li>
+                <li>{this.props.data.ingredient4}</li>
+                <li>{this.props.data.ingredient5}</li>
+                <li>{this.props.data.ingredient6}</li>
+            </ul></p>
+            <p className="lsDesc"><span>Опис страви:</span>{this.props.data.description}</p>
+                   </div>
     </div>
+    <div className="footer">
+        <button 
+            onClick={()=>this.setState({description:this.state.description?false:true})}
+            >{this.state.description? 'Cкрыть':'Приготування..'}</button>
+            {this.state.description && <p>{this.props.data.allDescription}</p>}
+    </div>
+    
+ </div>
+   
   );
 }
+}
 
-export default App;
+
